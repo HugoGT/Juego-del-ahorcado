afae544dfea01397a54620cf87682f80bb12e480
Hugo Gomez <gomezhugo2200t@gmail.com>
2021-08-07 14:25:39 -0500

    assert

--- debbuging.py
+++ debbuging.py
@@ -6,9 +6,14 @@ def divisors(num):
     return divisors
 
 def run():
-    num = int(input("Ingresa un número: "))
-    print(divisors(num))
-    print("Termino del programa")
+    try:
+        num = int(input("Ingresa un número: "))
+        if num < 0:
+            raise ValueError("No se pueden ingresar números negativos")
+        print(divisors(num))
+        print("Termino del programa")
+    except ValueError as ve:
+        print(ve)
 
 
 if __name__ == "__main__":
