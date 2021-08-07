5bb9fa20a9b11f3d39ee7ca516e3065af0c3cdd5
Hugo Gomez <gomezhugo2200t@gmail.com>
2021-08-07 11:57:13 -0500

    Empezando el curso de Python

---
+++ comprehensions.py
@@ -0,0 +1,17 @@
+from typing import Sequence
+
+
+def run():
+        # squares = []
+        # for i in range(1,101):
+        #     if i % 3 != 0:
+        #         squares.append(i**2)
+        # squares = [i**2 for i in range(1,101) if i % 3 != 0]
+        # print(squares)
+
+        squares = [i for i in range(1, 10000) if i % 36 == 0]
+        print(squares)
+
+
+if __name__ == "__main__":
+    run()
