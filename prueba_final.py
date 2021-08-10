1bac62c2c86c6e6eda8b58a746f23d0204a7779e
Hugo Gomez <gomezhugo2200t@gmail.com>
2021-08-10 12:19:47 -0500

    Dibujando el ahorcado

--- prueba_final.py
+++ prueba_final.py
@@ -2,7 +2,7 @@ from archives import write
 import random
 
 def randomize(numerated_data):
-    palabra_random = random.randint(171, 172)
+    palabra_random = random.randint(1, 172)
     for palabra in numerated_data:
         if palabra_random == palabra[0]:
             return palabra[1].replace("\n","")
@@ -16,7 +16,13 @@ def read():
     numerated_data = enumerate(data, 1)
     word_to_guess = randomize(numerated_data)
     print(word_to_guess)
-    print(len(word_to_guess))
+    spaces_to_draw = len(word_to_guess)
+    line = []
+    for i in range(0, spaces_to_draw):
+        line.append("_")
+    for j in range(0, spaces_to_draw):
+        print(line[j])
+    print(spaces_to_draw)
 
 
 def run():
