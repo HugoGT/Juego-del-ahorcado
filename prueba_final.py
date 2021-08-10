a69dbf576c931f4611b821112b2f45fcea67242f
Hugo Gomez <gomezhugo2200t@gmail.com>
2021-08-10 11:59:55 -0500

    Avanzando bien

--- prueba_final.py
+++ prueba_final.py
@@ -1,10 +1,11 @@
+from archives import write
 import random
 
-# def randomize(numerated_data):
-#     palabra_random = random.randint(1, 172)
-#     for palabra in numerated_data:
-#         if palabra_random == palabra[0]:
-#             return palabra
+def randomize(numerated_data):
+    palabra_random = random.randint(171, 172)
+    for palabra in numerated_data:
+        if palabra_random == palabra[0]:
+            return palabra[1].replace("\n","")
 
 
 def read():
@@ -13,11 +14,10 @@ def read():
         for line in f:
             data.append((line))
     numerated_data = enumerate(data, 1)
-    print("Return type: ", type(numerated_data))
-    palabra_random = random.randint(1, 172)
-    for palabra in numerated_data:
-        if palabra_random == palabra[0]:
-            print(palabra)
+    word_to_guess = randomize(numerated_data)
+    print(word_to_guess)
+    print(len(word_to_guess))
+
 
 def run():
     read()
