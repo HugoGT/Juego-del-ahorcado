90147a06f5c55a1c787d7314ff562f7f035d26c0
Hugo Gomez <gomezhugo2200t@gmail.com>
2021-08-14 14:45:56 -0500

    Menu Terminado

--- prueba_final.py
+++ prueba_final.py
@@ -1,7 +1,12 @@
-from archives import write
 from time import sleep
+from msvcrt import getch
 import random
 import os
+
+
+def clear_cls():    
+    clear_console = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+    clear_console()
 
 
 def give_the_word():
@@ -67,13 +72,49 @@ def welcome():
 
 
 def instructions():
-    print("""
-    Hola
-    Hola""")
+    clear_cls()
+    print("""Objetivo: Descubrir la palabra secreta.
+
+Preparación:
+- Al inicio el computador pensará en una palabra o frase y dibujará una línea por cada letra.
+- El jugador tiene 10 intentos antes de perder.
+
+Juego:
+- En su turno el jugador puede elegir una letra. 
+- El computador revisa si la letra pedida se encuentra en la palabra secreta.
+-- Si la letra está, entonces el computador la anota sobre la línea que ocupa su lugar en la palabra secreta.
+-- Si la letra no está, entonces el jugador perderá una vida.
+
+
+Fin de la partida:
+- GANA el adivinador si descubre la palabra secreta. 
+- GANA el computador si el adivinador pierde las 10 vidas antes de adivinar la palabra.
+
+""")
 
 
 def draw():
-    clear_console = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
+    clear_cls()
+    while True:
+        print(welcome())
+        option = int(input("\n                                      "))
+        if option == 1:
+            break
+        elif option == 2:
+            while True:
+                instructions()
+                print("Presione cualquier tecla para continuar")
+                getch()
+                break
+            break
+        elif option == 3:
+            exit()
+        else:
+            print("Escribe una opción correcta")
+            sleep(2)
+
+
+def game():
     word = give_the_word()
     line = word[0]
     letters = word[1]
@@ -81,24 +122,17 @@ def draw():
     for letter in letters:
         list_of_letters.append(letter)
     guessed_word = ""
-    while True:
-        clear_console()
-        print(welcome())
-        option = int(input("\n                                      "))
-        if option == 1:
-            break
-        elif option == 2:
-            instructions()
-            break
     for i in range(0,4):
         # clear_console()
         print(line)
         print(letters)
         print(list(list_of_letters), "\n")
+        break
 
 
 def run():
     draw()
+    game()
 
 
 if __name__ == "__main__":
